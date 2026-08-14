4cc1b89559f631c43242c417e905cebd869bdb5e
ericj706 <ericj6215@gmail.com>
2026-08-14 17:18:11 +0900

    취합준비

--- day13_team/view/MainView.java
+++ day13_team/view/MainView.java
@@ -8,8 +8,7 @@ import day13_team.controller.BoardController;
 import day13_team.controller.QnaController;
 import day13_team.model.Dto.BoardDto;
 import day13_team.model.Dto.QnaDto;
-import day13_team.view.MainView.BoardView;
-import day13_team.view.MainView.QnaView;
+
 
 public class MainView {
     private MainView() {}
@@ -40,13 +39,8 @@ public class MainView {
     
 
     // 게시글관리
-    public static class BoardView {
-        private BoardView(){} // 1.
-        private Scanner scan = new Scanner(System.in);
-        private static final BoardView instance = new BoardView(); 
-        public static BoardView getInstance( ){ return instance; } 
+    
         private BoardController bc = BoardController.getInstance();
-
         private void boardMenu() {
         while (true) {
             System.out.println("\n--- [게시물 관리] ---");
@@ -59,15 +53,13 @@ public class MainView {
             else if (ch.equals("3")) { break; }
             else { System.out.println("잘못된 입력입니다."); }
         }
-    }
+    
 
     }
+    
+    
     // 문의글 관리
-    public static class QnaView {
-        private QnaView(){} // 1.
-        private Scanner scan = new Scanner(System.in);
-        private static final QnaView instance = new QnaView(); 
-        public static QnaView getInstance( ){ return instance; } 
+    
         private QnaController qc = QnaController.getInstance();
 
         private void qnaMenu() {
@@ -86,7 +78,7 @@ public class MainView {
         
         // [1] 저장 VIEW
         public void qnaSave( ){
-            Scanner scan = MainView.getInstance().scan;
+            
             System.out.print("문의글: ");   String 문의글 = scan.next();  // 1.1 저장할 자료 입력받기 
             System.out.print("작성자: ");   String 작성자 = scan.next();
             QnaDto qnaDto = new QnaDto(0, 문의글, 작성자); // 1.2 자료 객체화하기 , no(아무거나/사용X)
@@ -104,7 +96,7 @@ public class MainView {
 
         // [3] 개별수정 VIEW
         public void qnaUpdate( ){
-            Scanner scan = MainView.getInstance().scan;
+            
             System.out.print("수정할번호: ");   int 수정할번호 = scan.nextInt();
             System.out.print("수정할내용: ");   String 수정할내용 = scan.next();
             QnaDto qnaDto = new QnaDto(수정할번호, 수정할내용, null ); // writer 사용안함.null
@@ -115,12 +107,12 @@ public class MainView {
 
         // [4] 개별삭제 VIEW
         public void qnaDelete( ){
-            Scanner scan = MainView.getInstance().scan;
+            
             System.out.print("삭제할번호: ");   int 삭제할번호 = scan.nextInt();
             boolean result = qc.qnaDelete( 삭제할번호 ); // 매개변수가 1개 이므로 dto 없이
             if( result ){ System.out.println(">삭제 성공"); }
             else{ System.out.println(">삭제 실패(없는 번호)"); }
         }
-    }
 }
+
 
